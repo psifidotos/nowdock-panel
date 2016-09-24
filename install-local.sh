1150d2574335f933177c0d6f159579b8c8f9b4c4
Michail Vourlakos <mvourlakos@gmail.com>
2016-09-24 21:24:57 +0300

    create installation scripts for easy to use

---
+++ install-local.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Author: Michail Vourlakos
+#Summary: Installation script for Now Dock Panel
+#This script was written and tested on openSuSe Leap 42.1
+
+cd nowdockpanel
+plasmapkg2 -i .
