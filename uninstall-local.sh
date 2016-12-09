414833e6d7f3e6aba1c33d8b98e1d28505e31d8c
Michail Vourlakos <mvourlakos@gmail.com>
2016-12-09 21:46:04 +0200

    include plasmoid in uninstall local script

--- uninstall-local.sh
+++ uninstall-local.sh
@@ -4,5 +4,6 @@
 #This script was written and tested on openSuSe Leap 42.1
 
 plasmapkg2 -r org.kde.store.nowdock.panel
+plasmapkg2 -r org.kde.store.nowdock.plasmoid
 plasmapkg2 -t layout-template -r org.kde.store.nowdock.defaultPanel
 plasmapkg2 -t layout-template -r org.kde.store.nowdock.emptyPanel
