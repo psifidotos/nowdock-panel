bbb41f69db748eae0102dc4f503e0ae214cf9cb4
Michail Vourlakos <mvourlakos@gmail.com>
2016-12-23 15:31:03 +0200

    update installation script, build only changes

--- install-global.sh
+++ install-global.sh
@@ -2,17 +2,13 @@
 #Author: Michail Vourlakos
 #Summary: Installation script for Now Dock Panel
 #This script was written and tested on openSuSe Leap 42.1
-
 set -e
 
-if [ -d build ]; then
-    cd build
-    rm -fr *
-else
+if ! [ -a build ] ; then
     mkdir build
-    cd build
 fi
 
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RELEASE ..
 make
 sudo make install
