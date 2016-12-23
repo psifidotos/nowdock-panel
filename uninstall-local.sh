944eb0543c46552c6444f5e0f20a1837102148db
Michail Vourlakos <mvourlakos@gmail.com>
2016-12-23 15:32:14 +0200

    simplify installation scripts naming

--- uninstall-local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Author: Michail Vourlakos
-#Summary: Installation script for Now Dock Panel
-#This script was written and tested on openSuSe Leap 42.1
-
-plasmapkg2 -r org.kde.store.nowdock.panel
-plasmapkg2 -r org.kde.store.nowdock.plasmoid
-plasmapkg2 -t layout-template -r org.kde.store.nowdock.defaultPanel
-plasmapkg2 -t layout-template -r org.kde.store.nowdock.emptyPanel
